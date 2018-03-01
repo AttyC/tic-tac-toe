27cac85a2ce5c66f08b2cd33c26e6af9d4f30889
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-01 15:01:15 +0000

    change Game tests to Board tests

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -1,15 +1,15 @@
-describe 'Game' do
-  # understands how to start a Game
+describe Board do
+  # understands how to start a Board
 
   describe '#display_board' do
-    it 'field starts empty' do
-      game = Game.new
-      expect(game.fields['0']).to eq nil
+    it 'field starts without moves' do
+      board = Board.new
+      expect(board.fields[0]).to eq '___'
     end
 
     it 'displays game board' do
-      game = Game.new
-      expect(game.display_board).to eq "___|___|___\n___|___|___\n___|___|___"
+      board = Board.new
+      expect(board.display_board).to eq "___|___|___\n___|___|___\n___|___|___"
     end
   end
 end
