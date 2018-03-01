b20f5d817174c03afc1473615745e5820ce19cef
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-01 14:44:34 +0000

    add display_board test / method

--- lib/game.rb
+++ lib/game.rb
@@ -4,18 +4,32 @@ class Game
 
   def initialize
     @fields = {
-      0 => nil,
-      1 => nil,
-      2 => nil,
-      3 => nil,
-      4 => nil,
-      5 => nil,
-      6 => nil,
-      7 => nil,
-      8 => nil,
-      9 => nil
+      1 => "___",
+      2 => "___",
+      3 => "___",
+      4 => "___",
+      5 => "___",
+      6 => "___",
+      7 => "___",
+      8 => "___",
+      9 => "___"
     }
 
   end
 
+  def display_board
+
+    board =
+    "#{@fields[1]}|" +
+     "#{@fields[2]}|"+
+     "#{@fields[3]}\n"+
+     "#{@fields[4]}|"+
+     "#{@fields[5]}|"+
+     "#{@fields[6]}\n"+
+     "#{@fields[7]}|"+
+     "#{@fields[8]}|"+
+     "#{@fields[9]}"
+     print board
+     board
+  end
 end
--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -1,9 +1,15 @@
 describe 'Game' do
+  # understands how to start a Game
 
   describe '#display_board' do
     it 'field starts empty' do
       game = Game.new
       expect(game.fields['0']).to eq nil
+    end
+
+    it 'displays game board' do
+      game = Game.new
+      expect(game.display_board).to eq "___|___|___\n___|___|___\n___|___|___"
     end
   end
 end
